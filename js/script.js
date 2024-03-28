6d9cc3427ae8dfff4ef2229383f57781882ea6e2
fabioqueiroz1415 <fabioaureliodedeus@gmail.com>
2024-03-28 19:18:06 -0300

    alterações

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const coelho = document.getElementById("coelho")
-coelho.addEventListener("click", mouseClickedCoelho);
 
 function mouseClickedCoelho(event) {
     if(coelho.classList.contains("coelho-visivel")) {
@@ -12,14 +11,53 @@ function mouseClickedCoelho(event) {
 }
 
 const egg1 = document.getElementById("egg1");
-egg1.addEventListener("click", mouseClickedEgg1);
 
 function mouseClickedEgg1(event) {
-    if(event.target.id === "metade-egg" || event.target.id === "rachado") {
-        egg1.style.transform = "scale(1.1)";
+    egg1.style.transform = "scale(1.1)";
 
+    setTimeout(function() {
+        egg1.style.transform = "scale(1)";
+    }, 300);
+}
+
+const envelope = document.getElementById("envelope");
+envelope.addEventListener("click", mouseClickedEnvelope);
+var isClickedEnvelop = 0;
+
+function mouseClickedEnvelope() {
+    if(isClickedEnvelop === 0) {
+        envelope.classList.toggle('tira-envelope');
+        isClickedEnvelop = -1;
         setTimeout(function() {
-            egg1.style.transform = "scale(1)";
-        }, 300);
+            isClickedEnvelop = 1;
+        }, 2000);
+    } else if(isClickedEnvelop === 1) {
+        if(isClickedCarta) {
+            carta.classList.toggle('dobrado');
+            isClickedCarta = false;
+        }
+        envelope.classList.toggle('coloca-envelope');
+        isClickedEnvelop = -1;
+        setTimeout(function() {
+            isClickedEnvelop = 0;
+        }, 2000);
+    }
+}
+
+const carta = document.getElementById("parte-baixo");
+carta.addEventListener("click", mouseClickedCarta);
+var isClickedCarta = false;
+
+function mouseClickedCarta(event) {
+    //onclick="document.getElementById('parte-baixo').classList.toggle('dobrado')"
+    if(!isClickedCarta) {
+        carta.classList.toggle('dobrado');
+        isClickedCarta = true;
+    } else {
+        if(event.target.id === "metade-egg" || event.target.id === "rachado") {
+            mouseClickedEgg1(event);
+        } else if(event.target.id === "coelho") {
+            mouseClickedCoelho(event);
+        }
     }
 }
